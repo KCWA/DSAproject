6dc722187e05fbd4e6ffee8d504126ef653f3ef3
ineshKCWA <Imesha@192.168.1.2>
2014-03-03 21:21:39 +0530

    add delete method

--- DSA project/src/BinTree.java
+++ DSA project/src/BinTree.java
@@ -49,6 +49,77 @@ public class BinTree {
 			System.out.println("Key is "+input.key);
 			
 	}
+	Node current=root;
+	Node parent=root;
+	boolean isLeftch=true;
+	public Node findDelNode(int delKey){
+		
+		while(current.getKey()!=delKey){	//go through it and search
+			parent=current;
+			if(delKey<current.getKey()){	//go to left subtree
+				isLeftch=true;
+				current=current.leftChld;
+			}else{							//or go to right sub tree
+				isLeftch=false;
+				current=current.rightChld;
+			}
+			if(current==null){				//if didn't find
+				return null;		
+			}				
+		}			//end of while
+		return current;
+	}
 	
 	
+	public boolean delete(Node current){
+		if(current.leftChld==null&& current.rightChld==null){	// if node is leaf
+			if(current==root)
+				root=null;
+			else if(isLeftch)
+				parent.leftChld=null;
+			else
+				parent.rightChld=null;
+		}else if(current.rightChld==null){	//if right child is null,replace leftsubtree
+			if(current==root)
+				root=current.leftChld;
+			else if(isLeftch)				//left child of parent
+				parent.leftChld=current.leftChld;
+			else							//right child of parent
+				parent.leftChld=current.rightChld;
+		}else if(current.rightChld==null){  //if left child is null,replace rightsubtree
+			if(current==root)
+				root=current.rightChld;
+			else if(isLeftch)
+				parent.leftChld=current.rightChld;
+			else
+				parent.rightChld=current.rightChld;				
+		}else{								// has two children
+			Node successor=getSuccessor(current);		//call the function to find successor
+			if(current==root)
+				root=successor;
+			else if(isLeftch)
+				parent.leftChld=successor;
+			else
+				parent.rightChld=successor;
+			successor.leftChld=current.leftChld;
+		}
+		
+	return true;
+	}
+	
+	private Node getSuccessor(Node delNode){
+		Node sucsParent=delNode;
+		Node sucssr=delNode;
+		Node crnt=delNode.rightChld;		//go to right side
+		while(crnt!=null){					//untill find node without leftchild
+			sucsParent=sucssr;
+			sucssr=crnt;
+			crnt=crnt.leftChld;				//go to left
+		}
+		if(sucssr!=delNode.rightChld){
+			sucsParent.leftChld=sucssr.rightChld;
+			sucssr.rightChld=delNode.rightChld;
+		}
+		return sucssr;
+	}
 }

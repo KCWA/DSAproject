d430104cb281d4936bbfab1492c6ff5f2ba55d6e
ineshKCWA <ineshgunasekara@ymail.com>
2014-03-05 08:52:36 +0530

    final class

--- DSA project/src/Node.java
+++ DSA project/src/Node.java
@@ -1,14 +1,13 @@
-
-public class Node {
+public class Node  {
 	String title;
 	String autFname;
 	String autLname;
 	int isbn;
-	int key;
+	int key;		// to store by name ,generate number from title
 	Node leftChld;
 	Node rightChld;
 	
-	public Node(String title, String autFname, String autLname, int isbn) {
+	public Node(String title, String autFname, String autLname, int isbn) {			
 		
 		this.title = title;
 		this.autFname = autFname;
@@ -16,24 +15,27 @@ public class Node {
 		this.isbn = isbn;
 		this.key = hashcode(title);
 	}
-	private int hashcode(String name) {
+	public Node() {			//Default constructor
+		
+	}
+	public int hashcode(String name) {		// generate unique number for each  book 
 		int hash=500;
 		name=name.toLowerCase();
 		int len=name.length();
 		int cnt=name.indexOf("java");
 		int c0=name.indexOf("a");
 		if(cnt>=0){
-			System.out.println("book contain java "+cnt);
+//			System.out.println("book contain java "+cnt);
 			hash-=cnt;
 			hash=hash-len-c0;
 		}else{
 			cnt=name.indexOf("php");
 			if(cnt>=0){
-				System.out.println("book contain php "+cnt);
+//				System.out.println("book contain php "+cnt);
 				hash+=cnt;
 				hash=hash+len+c0;
 			}else{
-				System.out.println("book contain other "+cnt);
+//				System.out.println("book contain other "+cnt);
 				hash+=500;
 			}
 		}
@@ -43,7 +45,7 @@ public class Node {
 		return key;
 	}	
 	
-	public int hashcode(int i){
+	public int hashcode(int i){				//for root of the tree
 		return i;
 	}
 	

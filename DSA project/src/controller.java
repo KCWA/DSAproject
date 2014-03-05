8687439b9a17815d3e2b7561c201e90a8ecf40de
ineshKCWA <ineshgunasekara@ymail.com>
2014-03-05 08:56:34 +0530

    final class

--- DSA project/src/controller.java
+++ DSA project/src/controller.java
@@ -1,22 +1,22 @@
-public class controller {
-  BinTree bn;	
-	public controller() {		
+
+public class controller {	
+	BinTree bn;				
+	public controller() {		//constructor create object of BinTree and make link
 		bn = new BinTree();
 	}
+
 	public boolean inputBook(String title, String autFname, String autLname, int isbn){
 		
-		boolean bkTypeJv=checkBookCntnt(title, "java");
+		boolean bkTypeJv=checkBookCntnt(title, "java");		//check the type of book 
 		boolean bkTypeph=checkBookCntnt(title, "php");
-		if(bkTypeJv!=false||bkTypeph!=false){
-			//System.out.println("==============book contain java or php"+isbn);
-		
-				boolean cntainIsbn=bn.checkISBN(bn.root, isbn);
-//					System.out.println("checking isbn--------        "+isbn+cntainIsbn);
+		if(bkTypeJv!=false||bkTypeph!=false){				// books contain java or php
+				boolean cntainIsbn=bn.checkISBN(bn.root, isbn);		//check isbn is already taken
+
 					if(!cntainIsbn){
-								Node newNode=new Node(title, autFname, autLname, isbn);
-								bn.insert(newNode);
+								Node newNode=new Node(title, autFname, autLname, isbn);		//create a new object
+								bn.insert(newNode);											// add the object to binary tree
 						}else
-							System.out.println("book already have isbn");
+							System.out.println("book ISBN is Already taken");
 						
 						
 						return true;
@@ -29,10 +29,19 @@ public class controller {
 		System.out.println("******************************current book list***********************************");
 		bn.inOrder(bn.root);
 	}
+	public boolean checkBookCntnt(String name,String type){
+		name=name.toLowerCase();		
+		int cntn=name.indexOf(type);	//chen if name contain type
+		if(cntn>=0)
+			return true;
+		
+		return false;
+	}
+	
 	public void searchName(String name){
 		Node n=new Node();
-		int hash=n.hashcode(name);
-		n=bn.findDelNode(hash);
+		int hash=n.hashcode(name);		//generate hash code using node class
+		n=bn.findDelNode(hash);			
 		if(n==null)
 			System.out.println("No book named  "+name);
 		else if(n.title.equalsIgnoreCase(name))
@@ -42,49 +51,57 @@ public class controller {
 	}
 	public void searchISBN(int isbn){
 		Node n=new Node();
-		n=bn.checkISBNDel(bn.root, isbn);
+		n=bn.checkISBNDel(bn.root, isbn);	
 		if(n==null)
-			System.out.println("No book named  "+isbn);
+			System.out.println("No book Contain ISBN  "+isbn);
 		else if(n.isbn==isbn)
 			BinTree.printOut(n);
 		else
-			System.out.println("No book named  "+isbn);
+			System.out.println("No book Contain ISBN  "+isbn);
 		
 	}
 	public void printRelTag(String tag){
 		bn.printRelate(bn.root, tag);		
 	}
 	public void delByName(String name){
-        Node node=new Node();
-        int key=node.hashcode(name);
-        System.out.println(key);
-        node=bn.findDelNode(key);
-        if(node!=null){
-            boolean cnfrm=bn.deleteNode(node);
-                    if(cnfrm){
-                        BinTree.printOut(node);
-                        System.out.println("The record is deleted");
-                    }
-                    else
-                        System.out.println("The record deleted");
-        }else
-            System.out.println("Sorry No book named "+name);
-       
-    }
-    public void delByIsbn(int isbn){
-        Node node=new Node();       
-        node=bn.checkISBNDel(bn.root, isbn);
-        if(node!=null){
-            boolean cnfrm=bn.deleteNode(node);
-                    if(cnfrm){
-                        BinTree.printOut(node);
-                        System.out.println("The record is deleted");
-                    }
-                    else
-                        System.out.println("The record deleted");
-        }else
-            System.out.println("Sorry No book contain "+isbn);
-       
-   
-    }
+		Node node=new Node();
+		int key=node.hashcode(name);	// generate hash code using node class
+
+		node=bn.findDelNode(key);		//Traverse the tree and find the node(book object) return that object
+		if(node!=null){
+			boolean cnfrm=bn.deleteNode(node);	//send that object to delete
+					if(cnfrm){
+						BinTree.printOut(node);			//if deleted show the result
+						System.out.println("The record is deleted");
+					}
+					else
+						System.out.println("The record deleted");
+		}else
+			System.out.println("Sorry No book named "+name);
+		
+	}
+	public void delByIsbn(int isbn){
+		Node node=new Node();	
+		bn.match=null;
+		node=bn.checkISBNDel(bn.root, isbn);	//travese in tree and take the object that contain same isbn
+		if(node!=null){			
+			boolean cnfrm=bn.deleteNode(node);	//send that object to delete
+					if(cnfrm){
+						BinTree.printOut(node);
+						System.out.println("The record is deleted");
+						node=null;					
+					}
+					else
+						System.out.println("The record is not deleted");
+		}else
+			System.out.println("Sorry No book contain "+isbn);	
+		node=null;
+	}	
+	public boolean isEmpty(){
+		if(bn.root== null)
+			return false;
+		else
+			return true;
+	}
+	
 }

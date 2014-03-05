785095a178b4d0d2c670befda7862d8f7630b407
ineshKCWA <ineshgunasekara@ymail.com>
2014-03-05 08:57:57 +0530

    final class
    ( now don't need use this class while implementing to test

--- DSA project/src/test.java
+++ DSA project/src/test.java
@@ -1,7 +1,7 @@
 
 
 public class test {
-
+/*
 	public static void main(String[] args) {
 		BinTree bt=new BinTree();
 		Node nod=new Node("java programing", "man", "test", 120);
@@ -10,13 +10,16 @@ public class test {
 		nod=new Node(" help php", "ben", "log", 1524);
 		bt.insert(nod);
 		
+		nod=new Node(" help java", "ben", "log", 1320);
+		bt.insert(nod);
+		
 		nod=new Node("php help", "benten", "log laf", 1523);
 		bt.insert(nod);
 		
-		nod=new Node("java help", "katty ", "sierra", 1523);
+		nod=new Node("java help", "katty ", "sierra", 1528);
 		bt.insert(nod);
 		
-		nod=new Node("java", "katy ", "Sierra", 1205);
+		nod=new Node("java", "katy ", "Sierra", 1255);
 		bt.insert(nod);
 		
 		nod=new Node("java fx", "katy sys ", "Sierrasdfdf", 1206);
@@ -24,12 +27,34 @@ public class test {
 		
 		nod=new Node("php", "inesh ", "dhanushka", 1205);
 		bt.insert(nod);
-		nod=new Node("php with html", "ineshda ", "gundhanushka", 1255);
+		nod=new Node("php with html", "ineshda ", "gundhanushka", 1295);
 		bt.insert(nod);
 		
+		
+		bt.inOrder(bt.root);
+		/*Node n=bt.findDelNode(500);
+		System.out.println("================find the node");
+		bt.printOut(n);
+		boolean b=bt.deleteNode(n);
+		System.out.print(b+"+++++++++++++++++++++++++++++++++++");
 		bt.inOrder(bt.root);
 		
+		Node del=bt.checkISBNDel(bt.root, 120);
+		if(del!=null){
+			System.out.println(bt.deleteNode(del)+">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		}
+		/*
 		
+		System.out.println("current key is "+bt.current.key);
+		System.out.println("parent key is "+bt.parent.key);
+		
+		
+		System.out.println("after delete");
+		bt.inOrder(bt.root);
+		
+		//bt.printRelate(bt.root, "php");
+		controller cnt=new controller();
+		cnt.inputBook("php programming", "autFname", "autLname", 1206);
 	}
-
+*/
 }
